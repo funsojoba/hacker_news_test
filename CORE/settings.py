2d6eb520172c32baf4a52e5fe5265fbcb9c718d0
MAC <hrfunsojoba@gmail.com>
2023-07-01 23:00:38 +0100

    auth styling

--- CORE/settings.py
+++ CORE/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news_app'
+    'news_app',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -119,8 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-
+STATICFILES_DIRS = [str(BASE_DIR / "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
